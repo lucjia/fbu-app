60cf713de32870f24ad089f8f26f7a7e6f9e735b
Lucy Jiang <lucjia@fb.com>
2019-07-15 14:02:08 -0700

    Initial Commit

---
+++ fbu-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fbu-app
+//
+//  Created by lucjia on 7/15/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fbu-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fbu-app
+//
+//  Created by lucjia on 7/15/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
