9a4c10f647bb275de5f55632f8d3a0f2c266a26c
Lucy Jiang <lucjia@fb.com>
2019-07-16 10:05:06 -0700

    Created Register VC file, double checking branching

---
+++ fbu-app/RegisterViewController.h
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.h
+//  fbu-app
+//
+//  Created by lucjia on 7/16/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RegisterViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
