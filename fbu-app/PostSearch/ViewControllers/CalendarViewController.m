73edcc0d065f7e838141ab8330596f103802cced
Jordan Foster <jordan487@fb.com>
2019-07-30 15:24:46 -0700

    changed properties to instance variables in CalendarViewController

--- fbu-app/PostSearch/ViewControllers/CalendarViewController.m
+++ fbu-app/PostSearch/ViewControllers/CalendarViewController.m
@@ -12,19 +12,20 @@
 #import "Event.h"
 
 @interface CalendarViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+{
+    UICollectionView *collectionView;
+    NSArray *eventsArray;
+    NSDate *eventDate;
+    NSInteger currentDay;
+    NSInteger currentMonth;
+    NSInteger currentYear;
+    NSInteger weekday; // weekday of start of month (can be 1 - 7)
+    NSDate *numberOfDays; // in month
+    NSDate *currentDate; // today
+    NSCalendar *calendar;
+}
 
-@property NSInteger currentDay;
-@property NSInteger currentMonth;
-@property NSInteger currentYear;
-@property NSInteger weekday; // weekday of start of month (can be 1 - 7)
-@property (strong, nonatomic) NSDate *numberOfDays; // in month
-@property (strong, nonatomic) NSDate *currentDate; // today
-@property (strong, nonatomic) NSCalendar *calendar;
 @property (weak, nonatomic) IBOutlet UILabel *monthLabel;
-@property (strong, nonatomic) UICollectionView *collectionView;
-@property (strong, nonatomic) NSArray *eventsArray;
-@property (strong, nonatomic) NSDate *eventDate;
-
 @end
 
 @implementation CalendarViewController
@@ -37,7 +38,7 @@
     [self initCollectionView];
     [self initCalendar:[NSDate date]];
     [self setMonthLabelText];
-    [self.collectionView reloadData];
+    [collectionView reloadData];
 }
 
 - (void)fetchEvents {
@@ -56,7 +57,7 @@
     [query findObjectsInBackgroundWithBlock:^(NSArray *events, NSError *error) {
         if (events != nil) {
             // do something with the array of object returned by the call
-            self.eventsArray = events;
+            self->eventsArray = events;
            [self initCollectionView];
         } else {
             NSLog(@"%@", error.localizedDescription);
@@ -72,48 +73,48 @@
 - (void)initCollectionView {
     UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
     CGFloat yPostion = 120;
-    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, yPostion, self.view.bounds.size.width, self.view.bounds.size.height - yPostion) collectionViewLayout:layout];
-    [self.collectionView setDataSource:self];
-    [self.collectionView setDelegate:self];
+    collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, yPostion, self.view.bounds.size.width, self.view.bounds.size.height - yPostion) collectionViewLayout:layout];
+    [collectionView setDataSource:self];
+    [collectionView setDelegate:self];
     
-    [self.collectionView registerClass:[CalendarCell class] forCellWithReuseIdentifier:@"CalendarCell"];
-    [self.collectionView setBackgroundColor:[UIColor whiteColor]];
+    [collectionView registerClass:[CalendarCell class] forCellWithReuseIdentifier:@"CalendarCell"];
+    [collectionView setBackgroundColor:[UIColor whiteColor]];
     
     layout.minimumInteritemSpacing = 5;
     layout.minimumLineSpacing = 5;
     CGFloat postersPerLine = 7;
-    CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumLineSpacing * (postersPerLine - 1)) / postersPerLine;
+    CGFloat itemWidth = (collectionView.frame.size.width - layout.minimumLineSpacing * (postersPerLine - 1)) / postersPerLine;
     CGFloat itemHeight = 1.5 * itemWidth;
     layout.itemSize = CGSizeMake(itemWidth, itemHeight);
     
-    self.collectionView.contentInsetAdjustmentBehavior = NO;
+    collectionView.contentInsetAdjustmentBehavior = NO;
     
-    [self.view addSubview:self.collectionView];
+    [self.view addSubview:collectionView];
 }
 
 // initialized the calendar
 - (void)initCalendar:(NSDate *)date {
-    self.calendar = [NSCalendar currentCalendar];
+    calendar = [NSCalendar currentCalendar];
     // allows for a date to specified in specific units, day, month, year, etc.
-    NSDateComponents *dateComponent = [self.calendar components:NSCalendarUnitCalendar |
+    NSDateComponents *dateComponent = [calendar components:NSCalendarUnitCalendar |
                                                                  NSCalendarUnitYear |
                                                                  NSCalendarUnitMonth |
                                                                  NSCalendarUnitDay |
                                                                  NSCalendarUnitWeekday fromDate:date];
-    self.currentMonth = [dateComponent month];
-    self.currentDay = [dateComponent day];
-    self.currentYear = [dateComponent year];
-    if (!self.currentDate) {
-        self.currentDate = [self.calendar dateFromComponents:dateComponent];
+    currentMonth = [dateComponent month];
+    currentDay = [dateComponent day];
+    currentYear = [dateComponent year];
+    if (!currentDate) {
+        currentDate = [calendar dateFromComponents:dateComponent];
     }
 
-    [self startOfMonthForCalendar:self.calendar dateComponent:dateComponent];
+    [self startOfMonthForCalendar:calendar dateComponent:dateComponent];
 }
 
 - (void)setMonthLabelText {
     NSDateFormatter *df = [[NSDateFormatter alloc] init];
-    NSString *monthName = [[df monthSymbols] objectAtIndex:(self.currentMonth - 1)];
-    NSString *completeString = [NSString stringWithFormat:@"%@ %ld", monthName, self.currentYear];
+    NSString *monthName = [[df monthSymbols] objectAtIndex:(currentMonth - 1)];
+    NSString *completeString = [NSString stringWithFormat:@"%@ %ld", monthName, currentYear];
     
     self.monthLabel.text = completeString;
 }
@@ -121,16 +122,16 @@
 // finds the first day of the current month and what day of the week it falls on
 - (void)startOfMonthForCalendar:(NSCalendar *)calendar dateComponent:(NSDateComponents *)component {
     [component setDay:1];
-    [component setMonth:self.currentMonth];
-    [component setYear:self.currentYear];
+    [component setMonth:currentMonth];
+    [component setYear:currentYear];
     NSDate *startDate = [calendar dateFromComponents:component];
-    component = [self.calendar components:NSCalendarUnitWeekday fromDate:startDate];
-    self.weekday = [component weekday];
+    component = [calendar components:NSCalendarUnitWeekday fromDate:startDate];
+    weekday = [component weekday];
 }
 
 // returns the number of days in the month for the date passed in
 - (NSInteger)numberDaysInMonthFromDate:(NSDate *)date {
-    NSRange range = [self.calendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:date];
+    NSRange range = [calendar rangeOfUnit:NSCalendarUnitDay inUnit:NSCalendarUnitMonth forDate:date];
     
     return range.length;
 }
@@ -146,14 +147,14 @@
 - (void)changeMonth:(NSInteger)month toMonth:(NSInteger)change changeBy:(NSInteger)delta {
     NSDateComponents *comps = [[NSDateComponents alloc] init];
     
-    if (self.currentMonth == month){
+    if (currentMonth == month){
         [comps setDay:1];
         [comps setMonth:change];
-        [comps setYear:self.currentYear + delta];
+        [comps setYear:currentYear + delta];
     } else {
         [comps setDay:1];
-        [comps setMonth:self.currentMonth + delta];
-        [comps setYear:self.currentYear];
+        [comps setMonth:currentMonth + delta];
+        [comps setYear:currentYear];
     }
     
     NSDate *date = [[NSCalendar currentCalendar] dateFromComponents:comps];
@@ -161,20 +162,20 @@
     [self initCollectionView];
     [self initCalendar:date];
     [self setMonthLabelText];
-    [self.collectionView reloadData];
+    [collectionView reloadData];
 }
 
 // checks to see if the cell in the calendar is today
 - (BOOL)isCellToday:(NSInteger)date {
-    NSDateComponents *dateComponent = [self.calendar components:NSCalendarUnitCalendar |
+    NSDateComponents *dateComponent = [calendar components:NSCalendarUnitCalendar |
                                        NSCalendarUnitYear |
                                        NSCalendarUnitMonth |
                                        NSCalendarUnitDay |
-                                       NSCalendarUnitWeekday fromDate:self.currentDate];
+                                       NSCalendarUnitWeekday fromDate:currentDate];
     
     return [dateComponent day] == date &&
-           [dateComponent month] == self.currentMonth &&
-           [dateComponent year] == self.currentYear;
+           [dateComponent month] == currentMonth &&
+           [dateComponent year] == currentYear;
 }
 
 - (NSDate *)dateWithYear:(NSInteger)year month:(NSInteger)month day:(NSInteger)day {
@@ -182,13 +183,13 @@
     [components setYear:year];
     [components setMonth:month];
     [components setDay:day];
-    return [self.calendar dateFromComponents:components];
+    return [calendar dateFromComponents:components];
 }
 
 - (BOOL)doesArrayContainDateOnSameDay:(NSArray *)array date:(NSDate *)date {
     for (int i = 0; i < array.count; i++) {
-        NSDate *event = [self.eventsArray[i] objectForKey:@"eventDate"];
-        if ([self.calendar isDate:date inSameDayAsDate:event]) {
+        NSDate *event = [eventsArray[i] objectForKey:@"eventDate"];
+        if ([calendar isDate:date inSameDayAsDate:event]) {
             return YES;
         }
     }
@@ -208,27 +209,27 @@
 
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     CalendarCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CalendarCell" forIndexPath:indexPath];
-    self.eventDate = [self dateWithYear:self.currentYear month:self.currentMonth day:indexPath.row - self.weekday + 2];
+    eventDate = [self dateWithYear:currentYear month:currentMonth day:indexPath.row - weekday + 2];
     
-    if (indexPath.item <= self.weekday - 2) {
+    if (indexPath.item <= weekday - 2) {
         [cell setHidden:YES];
-    } else if ([self doesArrayContainDateOnSameDay:self.eventsArray date:self.eventDate]) {
+    } else if ([self doesArrayContainDateOnSameDay:eventsArray date:eventDate]) {
         [cell setHidden:NO];
         cell.backgroundColor = [UIColor whiteColor];
         [cell drawEventCircle];
         
-        [cell initDateLabelInCell:(indexPath.row - self.weekday + 2)];
-    } else if ([self isCellToday:indexPath.row - self.weekday + 2]) {
+        [cell initDateLabelInCell:(indexPath.row - weekday + 2)];
+    } else if ([self isCellToday:indexPath.row - weekday + 2]) {
         [cell setHidden:NO];
         cell.backgroundColor = [UIColor whiteColor];
         [cell drawCurrentDayCircle];
         
-        [cell initDateLabelInCell:(indexPath.row - self.weekday + 2)];
+        [cell initDateLabelInCell:(indexPath.row - weekday + 2)];
     } else {
         [cell setHidden:NO];
         cell.backgroundColor = [UIColor whiteColor];
         
-        [cell initDateLabelInCell:(indexPath.row - self.weekday + 2)];
+        [cell initDateLabelInCell:(indexPath.row - weekday + 2)];
     }
     
     return cell;
@@ -237,7 +238,7 @@
 // returns the number of days in the current month + the day of the week the month starts on - 1 (for indexing starting at 0)
 // additional cells are created in order to have cells displayed on the calendar on the correct day of the week. ex: july starts on monday not sunday
 - (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return [self numberDaysInMonthFromDate:[NSDate date]] + (self.weekday - 1);
+    return [self numberDaysInMonthFromDate:[NSDate date]] + (weekday - 1);
 }
 
 @end
