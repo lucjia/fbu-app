76006a9d127e5a4681f20dea4cb65971ef4b341d
Lucy Jiang <lucjia@fb.com>
2019-07-30 17:10:53 -0700

    Added lock editing switch and stored its state to parse

--- fbu-app/ViewControllers/FeatureScreens/ComposeReminderViewController.m
+++ fbu-app/ViewControllers/FeatureScreens/ComposeReminderViewController.m
@@ -28,6 +28,7 @@
 @property (strong, nonatomic) IBOutlet UITapGestureRecognizer *tap;
 @property (strong, nonatomic) UserCollectionCell *previousCell;
 @property (assign, nonatomic) NSInteger cellHeight;
+@property (weak, nonatomic) IBOutlet UISwitch *lockEditingSwitch;
 
 @end
 
@@ -116,7 +117,7 @@
                     dueDate = nil;
                 }
                 
-                [Reminder createReminder:self.receiver text:self.reminderTextView.text dueDate:dueDate dueDateString:self.dueDateString withCompletion:nil];
+                [Reminder createReminder:self.receiver text:self.reminderTextView.text dueDate:dueDate dueDateString:self.dueDateString lockEditing:self.lockEditingSwitch.isOn withCompletion:nil];
             } else {
                 NSLog(@"%@", error.localizedDescription);
             }
