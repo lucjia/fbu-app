89ba93b615d314223e49a348d74bae21524d1055
Jordan Foster <jordan487@fb.com>
2019-08-02 10:00:26 -0700

    can select cells and color changes when selected

--- fbu-app/PostSearch/Cells/CalendarCell.h
+++ fbu-app/PostSearch/Cells/CalendarCell.h
@@ -16,6 +16,8 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)setCurrentDayTextColor;
 - (void)drawCurrentDayCircle;
 - (void)drawEventCircle;
+- (void)colorSelectedCell;
+- (void)decolorSelectedCell;
 
 @end
 
