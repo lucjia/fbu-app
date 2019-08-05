f2f88b6213ba909fa84d6d75064b06a6ad0f68ff
Jordan Foster <jordan487@fb.com>
2019-08-05 15:10:50 -0700

    created custom color and changed colors of calendar and swipe menu

---
+++ fbu-app/ColorPalette/CustomColor.h
@@ -0,0 +1,22 @@
+//
+//  CustomColor.h
+//  fbu-app
+//
+//  Created by jordan487 on 8/5/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomColor : UIView
+
++ (UIColor *)lightyellowGreen:(CGFloat)colorAlpha;
++ (UIColor *)lightGreen:(CGFloat)colorAlpha;
++ (UIColor *)Green:(CGFloat)colorAlpha;
++ (UIColor *)DarkGreen:(CGFloat)colorAlpha;
+
+@end
+
+NS_ASSUME_NONNULL_END
