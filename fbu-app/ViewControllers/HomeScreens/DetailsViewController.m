d58e566947b1c9029f4504b1e2f39188514cd984
Lucy Jiang <lucjia@fb.com>
2019-08-07 12:14:26 -0700

    Changed alerts to all fit the color theme

--- fbu-app/ViewControllers/HomeScreens/DetailsViewController.m
+++ fbu-app/ViewControllers/HomeScreens/DetailsViewController.m
@@ -114,6 +114,7 @@
     }];
     // add the OK action to the alert controller
     [alert addAction:okAction];
+    alert.view.tintColor = [CustomColor accentColor:1.0];
     
     [sender presentViewController:alert animated:YES completion:^{}];
 }
