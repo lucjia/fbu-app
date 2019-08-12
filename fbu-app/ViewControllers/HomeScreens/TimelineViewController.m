f952f8b6d247ae6cb14db7d003af3b4ae3e3d49b
Jordan Foster <jordan487@fb.com>
2019-08-12 16:59:13 -0700

    refresh controll on all three timelines

--- fbu-app/ViewControllers/HomeScreens/TimelineViewController.m
+++ fbu-app/ViewControllers/HomeScreens/TimelineViewController.m
@@ -41,7 +41,7 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
-    [self.tableView setBackgroundColor:[UIColor whiteColor]];
+    [self.tableView setBackgroundColor:[UIColor groupTableViewBackgroundColor]];
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     
     [SVProgressHUD show];
